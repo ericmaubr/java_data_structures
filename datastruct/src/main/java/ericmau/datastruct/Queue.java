dc5b54e57dae02093ea79415629577e7e68b8f54
erimaubr <ericmaubr@gmail.com>
2024-08-26 17:24:52 -0300

    javadoc Queue

--- datastruct/src/main/java/ericmau/datastruct/Queue.java
+++ datastruct/src/main/java/ericmau/datastruct/Queue.java
@@ -1,5 +1,29 @@
 package ericmau.datastruct;
 
+/**
+ * Implementation of the queue data structure using a generic type.
+ * 
+ * This class provides methods to enqueue, dequeue, and peek elements in a queue, as well as to check if the queue is empty.
+ * A queue follows the First In, First Out (FIFO) principle.
+ * 
+ * @param <T> the type of elements held in this queue
+ * 
+ * Example usage:
+ * To create and use a queue of type Integer:
+ * <pre>
+ *     Queue<Integer> queue = new Queue<>();
+ *     queue.enqueue(10);        // Adds 10 to the queue
+ *     queue.enqueue(20);        // Adds 20 to the queue
+ *     System.out.println(queue.peek()); // Prints the top element: 10
+ *     queue.dequeue();           // Removes the top element: 10
+ *     System.out.println(queue.isEmpty()); // Prints false if the queue still contains elements
+ * </pre>
+ * 
+ * @see Node
+ * 
+ */
+
+
 public class Queue<T> {
 
   protected Node<T> firstNode;
